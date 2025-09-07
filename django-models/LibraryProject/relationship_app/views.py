1753039fa8378d00903682763c823913ca3d5394
Elias <eliasgirma.h@gmail.com>
2025-09-07 23:07:00 +0300

    my

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Must import Library
+from .models import Book, Library 
+from .models import Library # Must import Library
 
 # Function-based view to list all books
 def list_books(request):
