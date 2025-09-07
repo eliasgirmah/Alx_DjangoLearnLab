219b9ace188cb498ccd80632c5f7d3ce65d9c532
Elias <eliasgirma.h@gmail.com>
2025-09-07 23:42:15 +0300

    add, delete book

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library
 from .forms import BookForm  # Make sure to create a ModelForm for Book
 
