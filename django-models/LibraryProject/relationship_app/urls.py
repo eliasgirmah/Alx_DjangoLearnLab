db0541f9774030e97e47f4a216ca962ad5914d19
Elias <eliasgirma.h@gmail.com>
2025-09-07 23:46:22 +0300

    add, delete book

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     # Book Views
     # -----------------------------
     path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),                  # <-- add_book
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),      # <-- edit_book
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),# <-- delete_book
+    path('books/add/', views.add_book, name='add_book'),               # <-- add_book
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),   # <-- edit_book
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 
     # -----------------------------
     # Library Views
