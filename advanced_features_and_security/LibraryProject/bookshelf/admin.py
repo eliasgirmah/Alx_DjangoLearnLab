ebed266580b0aab3cb7b7086870714d74be4bb20
Elias <eliasgirma.h@gmail.com>
2025-09-14 21:55:32 +0300

    advanced features and security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
+# -------------------------
+# CustomUser Admin
+# -------------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_of_birth")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active")}
+        ),
+    )
+
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+
+# Register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# -------------------------
+# Book Admin
+# -------------------------
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')        # removed publication_year
-    list_filter = ('title', 'author')         # removed publication_year
-    search_fields = ('title', 'author__name') # author__name allows searching by author's name
+    list_display = ('title', 'author')        
+    list_filter = ('title', 'author')         
+    search_fields = ('title', 'author')  # changed from author__name
