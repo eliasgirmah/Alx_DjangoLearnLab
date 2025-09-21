8e99906aaa4ff96925a65891fd2c89dde9ce18f7
Elias <eliasgirma.h@gmail.com>
2025-09-21 16:51:51 +0300

    Create the API View

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from.models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListAPIView):
+    """
+    API view to list all books.
+    """
+    queryset = Book.objects.all()           # Fetch all books from the database
+    serializer_class = BookSerializer       # Use BookSerializer to convert to JSON
