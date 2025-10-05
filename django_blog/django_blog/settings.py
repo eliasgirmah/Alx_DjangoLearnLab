c1bccefb0d38a444ae488ce7c71994cd86267133
Elias <eliasgirma.h@gmail.com>
2025-10-05 15:52:19 +0300

    model update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD': 'elias',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 
