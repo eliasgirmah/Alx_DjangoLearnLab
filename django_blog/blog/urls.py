87c5750a9d5f677cac3195ea16c9d7f671b658a6
Elias Girma <38812248+eliasgirmah@users.noreply.github.com>
2025-10-08 04:08:19 +0300

    Delete django_blog directory

--- django_blog/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('', lambda request: redirect('login')),  # Redirect root to login
-    path('accounts/', include('accounts.urls')),
-]
