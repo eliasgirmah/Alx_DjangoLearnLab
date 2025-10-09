109f31a8796a8ef0befb8174951cfabe91229f82
Elias <eliasgirma.h@gmail.com>
2025-10-09 11:18:31 +0300

    post management

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
